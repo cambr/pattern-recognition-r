1a2ee470b038569c20fe98468741f7f7193c602e
Linus Oleander <linus@oleander.nu>
2012-12-04 12:04:13 +0100

    Add worstChar and bestChar

--- ass1/main.r
+++ ass1/main.r
@@ -2,7 +2,7 @@ main = function() {
   data = read.csv("/Users/linus/Documents/Projekt/pattern-recognition-r/ass1/data.txt", header = TRUE)
   rows = nrow(data)
 
-  for (k in 1:15) {
+  for (k in 1:10) {
     res = smartKnn(data, k, rows)
     cat(sprintf("K=%d, R=%.3f\n", k, res))
   }
@@ -13,5 +13,43 @@ smartKnn = function(data, k = 1, rows = nrow(data), fraction = 0.25) {
   testIndexes = sample(indexes, fraction * rows)
   trainingIndexes = indexes[-testIndexes]
   ans = knn(data[trainingIndexes,-1], data[testIndexes,-1], data[trainingIndexes, 1], k)
+
+  result = c()
+  for (i in 1:length(ans)) {
+    o = as.character(data[testIndexes,1][i])
+    if(is.null(result[o])){ 
+      result[o] = 0 
+    }
+
+    if(is.na(result[o])){
+      result[o] = 0 
+    }
+
+    if(o == ans[i]){
+      result[o] = result[o] + 1
+    }
+  }
+
+  worst = Inf
+  best = 0
+  bestChar = NULL
+  worstChar = NULL
+
+  for (char in names(result)) {
+    amount = result[char]
+    res = amount / sum(data[testIndexes,1] == char)
+    cat(sprintf("%s => %.3f\n", char, res))
+    if(res > best) {
+      best = res
+      bestChar = char
+    }
+
+    if(res < worst) {
+      worst = res
+      worstChar = char
+    }
+  }
+
+  cat(sprintf("Worst (%.3f): %s, Best (%.3f): %s\n", worst, worstChar, best, bestChar))
   sum(data[testIndexes,1] == ans) / length(data[testIndexes,1])
 }
